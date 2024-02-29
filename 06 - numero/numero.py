6063f200602bca10b05fe95475e22bb5566d3912
felix1089 <98363493+felix1089@users.noreply.github.com>
2024-02-29 17:22:23 -0300

    vamos bien

---
+++ 06 - numero/numero.py
@@ -0,0 +1,18 @@
+
+numero = 2 #entero integer
+#decimal = 1.2 # float
+#imaginario = 2 + 2j #  raiz cuadrad
+
+numero += 5
+numero -= 5
+numero /= 5
+numero *= 5 
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3) # aca no me aparecen los decimales
+print(1 % 3) # aca me aparece con js el resto 
+print(8 ** 3) # exponente+
+print(numero)
