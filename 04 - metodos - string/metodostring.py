a61c66dfd58ec3bcacd8fb73fca36415f42779dd
felix1089 <98363493+felix1089@users.noreply.github.com>
2024-02-29 16:06:27 -0300

    Mensaje descriptivo de los cambios realizados en metodostring.py

--- 04 - metodos - string/metodostring.py
+++ 04 - metodos - string/metodostring.py
@@ -6,3 +6,5 @@ print(animal.lower())
 print(animal2.strip().capitalize())
 print(animal.title())
 print(animal2.strip()) # remueve espacios
+print(animal.rstrip()) # remueve espacios a la derecha
+print(animal.find("ch")) #este metodo devuelve el indece de la cadena
