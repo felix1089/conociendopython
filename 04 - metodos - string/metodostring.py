587a9ffecdcc3f37c24c4c92147a5646c75156e3
felix1089 <98363493+felix1089@users.noreply.github.com>
2024-02-29 16:37:30 -0300

    metodos

--- 04 - metodos - string/metodostring.py
+++ 04 - metodos - string/metodostring.py
@@ -8,3 +8,8 @@ print(animal.title())
 print(animal2.strip()) # remueve espacios
 print(animal.rstrip()) # remueve espacios a la derecha
 print(animal.find("ch")) #este metodo devuelve el indece de la cadena
+print(animal.find("felix")) # si no es}xite el carecter en cuestion me va aparecer un -1 de no lo encuentro  y si es positivo lo consigue
+print(animal.replace("ch", "fe")) # reemmplaza la el valor 
+print("ch" in animal) # esta validando si se encuentra te retorna un booleano 
+print("cha" not in animal) # valida sin no esta te retorna un booleno 
+
