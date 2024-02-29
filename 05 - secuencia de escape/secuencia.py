483dc6d42e0ebc8f78d861c22125cfc284b34fda
felix1089 <98363493+felix1089@users.noreply.github.com>
2024-02-29 16:46:42 -0300

    modulo

---
+++ 05 - secuencia de escape/secuencia.py
@@ -0,0 +1,9 @@
+
+# \"
+# \""
+# \\
+# \n
+    
+curso = "ultimate \"python\""  # cuando hay texto que no puedo acceder para modicar las comillas se coloca\ para decirle que no tome en cuenta
+curso1 = "ultimate \npython\"" # aparecen texto arriba y abajo
+print(curso1)
