a2807a769f7d3554de38bd7a84c8cb397555a608
felix1089 <98363493+felix1089@users.noreply.github.com>
2024-03-06 16:12:54 -0300

    calculator

--- 07 -funciones - numero/funciones.py
+++ 07 -funciones - numero/funciones.py
@@ -8,5 +8,7 @@ print(round(1.7))
 print(abs(-77+5)) # retorna el valor absoluto 
 print(math.ceil(1.1))
 print(math.floor(1.1))
-print(math.isnan(20)) # te dice si existe el number 
+print(math.isnan(20)) # te dice si existe el number reales
+print(math.pow(10, 3)) # eleva a la potencia
+print(math.sqrt(9)) #saca la raiz caudrada
 
---
+++ 08 - calculator/calculadora.py
@@ -0,0 +1,4 @@
+n1 = input("ingresa el primer numero ")
+
+
+print(n1)
